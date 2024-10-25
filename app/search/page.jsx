0efbcc2656fe41af9dd8d017d385ae75b0e18f36
Hsu <mtm.hsuthinzar@gmail.com>
2024-10-25 14:23:56 +0630

    search

--- app/search/page.jsx
+++ app/search/page.jsx
@@ -1,34 +1,35 @@
 import UserList from "@/components/UserList";
 import { UserSearch } from "lucide-react";
 import { Typography, Box } from "@mui/material";
+
 async function fetchUsers() {
   const res = await fetch(`https://dummyjson.com/users`);
-  return await res.json();
+  const data = await res.json();
+  return data.users;
 }
 
 export default async function Search({ searchParams }) {
   const { q } = searchParams;
 
   const allUsers = await fetchUsers();
-
-  const filteredUsers = allUsers.users.filter(
+  const filteredUsers = allUsers.filter(
     (user) =>
       user.firstName.toLowerCase().includes(q.toLowerCase()) ||
       user.lastName.toLowerCase().includes(q.toLowerCase())
   );
 
-  const viewType = "table";
-
   return (
     <Box>
       <Typography
         variant="h2"
-        className="text-lg font-bold mb-4 pb-2 border-b items-center justify-center flex gap-2"
+        className="text-2xl font-bold my-4 pb-2 border-b items-center justify-center flex gap-2"
       >
         <UserSearch />
-        Search : {q}
+        Search: {q}
       </Typography>
-      <UserList userlist={filteredUsers} viewType={viewType} />
+      <div className="flex gap-4 flex-wrap justify-evenly">
+        <UserList userlist={filteredUsers} viewType="table" />
+      </div>
     </Box>
   );
 }
